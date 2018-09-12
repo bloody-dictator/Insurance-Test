ebbb157a2e20dddea974e054c48a91d11502ac9e
bloody-dictator <thebloodydictator@gmail.com>
2018-09-13 04:20:05 +0700

    тестируется страхование на сайте сбербанка

---
+++ src/test/java/InsuranceSberbank.java
@@ -0,0 +1,67 @@
+import org.junit.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.*;
+
+import java.util.concurrent.*;
+
+/**
+ * Created by IntelliJ IDEA.<br>
+ * User: Alexey<br>
+ * Date: 11.09.2018<br>
+ * Time: 23:15<br>
+ * todo javadoc
+ */
+public class InsuranceSberbank {
+
+    WebDriver driver;
+    String BaseURL;
+
+    @Before
+    public void beforeSberTest(){
+        System.setProperty("webdriver.chrome.driver", "drv/chromedriver.exe");
+        BaseURL = "http://www.sberbank.ru/ru/person";
+
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get(BaseURL);
+    }
+
+    @Test
+    public void insuranceSberTest() {
+        //Нажать на "Страхование"
+        driver.findElement(By.xpath("//span[contains(text(), \"Страхование\")]")).click();
+
+        //Выбрать "Путешествия и покупки"
+        driver.findElement(By.xpath("//li [@class = \"lg-menu__sub-item\"]/*[contains(text(), \"Путешествия и покупки\")]")).click();
+
+        //Проверить наличие на странице заголовка "Страхование путешественников"
+        WebElement title = driver.findElement(By.xpath("//h3[contains(text(), \"Страхование путешественников\")]"));
+        Assert.assertEquals("Страхование путешественников", title.getText());
+
+        //Нажать на "Оформить Онлайн"
+
+        driver.findElement(By.xpath("(//a[contains(text(), \"Оформить онлайн\")])[1]")).click();
+
+        // На вкладке "Выбор полиса"  выбрать сумму страховой защиты – Минимальная
+        driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
+
+        driver.findElement(By.xpath("//div[text() = \"Минимальная\"]/..")).click();
+
+        //Нажать "Оформить"
+
+        driver.findElement(By.xpath("//span[text() = \"Оформить\"]")).click();
+
+
+
+
+
+
+    }
+
+    @After
+
+    public void afterSberTest(){
+
+    }
+}
